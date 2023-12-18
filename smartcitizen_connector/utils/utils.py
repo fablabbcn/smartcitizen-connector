8744810aaf9605cde4b14863003b08eeb1145fa3
oscgonfer <oscgonfer@gmail.com>
2023-12-18 21:06:32 +0100

    Add useful tools to utils.py

--- smartcitizen_connector/utils/utils.py
+++ smartcitizen_connector/utils/utils.py
@@ -106,6 +106,12 @@ def localise_date(date, timezone, tzaware=True):
 
     return result_date
 
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
 def std_out(msg: str,
     mtype: Optional[str] = None,
     force: Optional[bool] = False
@@ -218,3 +224,43 @@ def get_pt_temp(slot, sensor_id):
     result.append({f'PT1000_POS': {'kwargs': {'channel': pt1000plus}}})
 
     return result
+
+def find_by_field(models, value, field):
+    try:
+        item = next(model for _, model in enumerate(models) if model.__getattribute__(field) == value)
+    except StopIteration:
+        std_out(f'Column {field} not in models')
+        pass
+    else:
+        return item
+    return None
+
+def dict_fmerge(base_dct, merge_dct, add_keys=True):
+    """
+    Recursive dict merge.
+    From: https://gist.github.com/CMeza99/5eae3af0776bef32f945f34428669437
+    Parameters
+    ----------
+        base_dct: dict
+            Dict onto which the merge is executed
+        merge_dct: dict
+            Dict merged into base_dct
+        add_keys: bool
+            True
+            Whether to add new keys
+    Returns
+    -------
+        Updated dict
+    """
+    rtn_dct = base_dct.copy()
+    if add_keys is False:
+        merge_dct = {key: merge_dct[key] for key in set(rtn_dct).intersection(set(merge_dct))}
+
+    rtn_dct.update({
+        key: dict_fmerge(rtn_dct[key], merge_dct[key], add_keys=add_keys)
+        if isinstance(rtn_dct.get(key), dict) and isinstance(merge_dct[key], dict)
+        else merge_dct[key]
+        for key in merge_dct.keys()
+    })
+
+    return rtn_dct
