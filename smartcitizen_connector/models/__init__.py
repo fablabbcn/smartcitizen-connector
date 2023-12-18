638a437e5ed07d2d6dab076e67f3751ec91ac38a
oscgonfer <oscgonfer@gmail.com>
2023-12-18 21:06:57 +0100

    Update in imports

--- smartcitizen_connector/models/__init__.py
+++ smartcitizen_connector/models/__init__.py
@@ -1,2 +1,2 @@
-from .models import (Sensor, Measurement, Kit, Owner, Location,
+from .models import (Sensor, Measurement, Kit, Owner, Location, Metric,
                      HardwareInfo, HardwarePostprocessing, Postprocessing, Data, Device)
