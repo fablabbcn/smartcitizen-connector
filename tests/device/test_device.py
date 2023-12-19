45ff6c9e5da88066d04a2cf17b25d82d2a3c36de
oscgonfer <oscgonfer@gmail.com>
2023-12-19 10:34:13 +0100

    Change test device

--- tests/device/test_device.py
+++ tests/device/test_device.py
@@ -4,25 +4,25 @@ from smartcitizen_connector.utils import localise_date
 import asyncio
 
 def test_device():
-    id = 16549
-    freq = '1Min'
+    id = 16838
+    frequency = '1Min'
     resample = False
-    uuid = "d030cb8a-2c2a-429e-9f04-416888708193"
-    min_date = '2023-07-29T09:00:06Z'
+    uuid = "80e684e5-359f-4755-aec9-30fc0c84415f"
+    min_date = '2022-09-10T00:00:00Z'
     max_date = None
 
     d = SCDevice(id)
     asyncio.run(d.get_data(
         min_date = localise_date(min_date, d.timezone),
         max_date = localise_date(max_date, d.timezone),
-        freq = freq,
+        frequency = frequency,
         clean_na = None,
         resample = resample)
     )
 
     d0 = d.data.index[0].tz_convert('UTC').strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    assert d.json.id == 16549, resp.text
+    assert d.json.id == id, resp.text
     assert d.data is not None, resp.text
     assert d.json.uuid == uuid, resp.text
     assert d0 == min_date, resp.text
